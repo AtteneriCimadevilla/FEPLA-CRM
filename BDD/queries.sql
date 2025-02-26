7c4fc91370ea5242e23a027c70a69a05f1f8c964
Atteneri Maria Rodriguez Sanchez <attrodsan@outlook.com>
2025-02-26 22:51:39 +0100

    Conflicto en queries.sql.

--- BDD/queries.sql
+++ BDD/queries.sql
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
--- Active: 1738276149802@@127.0.0.1@3306@fepla_crm
-select * from profesores;
-=======
+
 select * from profesores;
 
 select * from empresas;
->>>>>>> d4676bb6aea2391486c8e2593252460fceaecb6f
+
